baa75c7f6307deb4eba61f471eda5b2e1b2df8e3
Volodymyr1910 <priestvolodya@gmail.com>
2024-12-18 15:04:56 +0100

    update

--- lessons/lessons_20/script.js
+++ lessons/lessons_20/script.js
@@ -1,19 +1,4 @@
-// fetch('https://get.geojs.io/v1/ip/geo.json')
-// .then(res => res.json())
-
-// .then(data => {
-// console.log(data)
-// const latitude = data.latitude;
-
-// const longitude = data.longitude;
-
-// const city = data.city;
-
-// // console.log(city)
-
-// })
-
-async function getCoordinates() {
+async function getCoordinatesByThen() {
   fetch("https://get.geojs.io/v1/ip/geo.json")
     .then((res) => res.json)
     .then((data) => {
@@ -31,6 +16,8 @@ async function getCoordinates() {
   const res = await fetch("https://get.geojs.io/v1/ip/geo.json");
   const data = await res.json();
 
+  getWeather(data);
+
   // робота з даними об'єкту без деструктуризації
   // const latitude = data.latitude;
   // const longitude = data.longitude;
@@ -39,7 +26,6 @@ async function getCoordinates() {
   // робота з даними об'єкту з деструктуризацією
   //const {latitude, longitude, city} = data
 
-  getWeather(data);
   // сделайте fetch запрос по адресу:
   // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true
   // вам нужно заменить координаты в строке на данные выше
@@ -110,9 +96,8 @@ async function getWeather(data) {
 
   console.log(weathercodeValue);
 
-  console.log(weathercode)
+  console.log(weathercode);
   const weathercodeWordsValue = weathercodeTranlation(weathercode);
-
 
   console.log("code", weathercodeWordsValue);
 
@@ -127,19 +112,19 @@ async function getWeather(data) {
 } // end async function getWeather(data)
 
 function weathercodeTranlation(weathercodeValue) {
-//   console.log(weathercodeValue);
-//   let weathercodeWordsValue = "";
+  //   console.log(weathercodeValue);
+  //   let weathercodeWordsValue = "";
   switch (weathercodeValue) {
     case 0:
       return "Clear sky";
     case 77:
-     return "Snow grains";
+      return "Snow grains";
     case 1:
       return "Mainly clear";
     case 2:
       return "Partly cloudy";
     case 3:
-     return "Overcast";
+      return "Overcast";
     case 45:
       return "Fog";
     case 48:
@@ -147,7 +132,7 @@ function weathercodeTranlation(weathercodeValue) {
     case 51:
       return "Light drizzle";
     case 53:
-     return "Moderate drizzle";
+      return "Moderate drizzle";
     case 55:
       return "Dense intensity drizzle";
     case 56:
@@ -161,13 +146,13 @@ function weathercodeTranlation(weathercodeValue) {
     case 65:
       return "Heavy intensity rain";
     case 66:
-     return "Light freezing rain";
+      return "Light freezing rain";
     case 67:
       return "Heavy intensity freezing rain";
     case 71:
       return "Slight snow fall";
     case 73:
-     return "Moderate snow fall";
+      return "Moderate snow fall";
     case 75:
       return "Heavy intensity snow fall";
     case 80:
@@ -175,7 +160,7 @@ function weathercodeTranlation(weathercodeValue) {
     case 81:
       return "Moderate rain showers";
     case 82:
-     return "Violent rain showers";
+      return "Violent rain showers";
     case 85:
       return "Slight snow showers";
     case 86:
@@ -185,7 +170,7 @@ function weathercodeTranlation(weathercodeValue) {
     case 96:
       return "Thunderstorm with slight ";
     case 99:
-     return "Thunderstorm with heavy hail";
+      return "Thunderstorm with heavy hail";
   } //switch (weathercodeValue)
 
   //if(weathercodeValue === 1 || weathercodeValue === 2 || weathercodeValue === 3) weathercodeWordsValue = "Mainly clear, partly cloudy, and overcast";
@@ -200,7 +185,7 @@ function weathercodeTranlation(weathercodeValue) {
   // if(weathercodeValue === 85 || weathercodeValue === 86) weathercodeWordsValue = "Snow showers slight and heavy";
   // if(weathercodeValue === 96 || weathercodeValue === 63 || weathercodeValue === 99) weathercodeWordsValue = "Thunderstorm with slight and heavy hail";
 
-//   return weathercodeWordsValue;
+  //   return weathercodeWordsValue;
 } //end function  weathercodeTranlation
 
 function createInPage(
